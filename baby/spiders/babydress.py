58eeb4cd21fbcaad44338a84d8c7d926c0db1b12
Administrator <cxo114.com@163.com>
2018-08-03 07:06:03 +0800

    crawlspider

--- baby/spiders/babydress.py
+++ baby/spiders/babydress.py
@@ -1,19 +1,35 @@
 # -*- coding: utf-8 -*-
 import scrapy
-from baby.items import BabyItem
+from baby.items import BabyItem,BabyDetailItem
 from scrapy.utils.response import get_base_url
 from scrapy.loader import ItemLoader
 from scrapy.loader.processors import TakeFirst
+from scrapy.spiders import CrawlSpider,Rule
+from scrapy.linkextractors import LinkExtractor
 #item loader
 class DefaultItemLoader(ItemLoader):
     default_output_processor = TakeFirst()
 
-class BabydressSpider(scrapy.Spider):
+class BabydressSpider(CrawlSpider):
     name = 'babydress'
     allowed_domains = ['babyonlinedress.cn']
-    start_urls = ['http://www-test.babyonlinedress.cn/accessories-c147/']
+    # accessories - c147 /
+    start_urls = ['http://www-test.babyonlinedress.cn/']
+    rules = (
+        Rule(LinkExtractor(allow=('http://www-test.babyonlinedress.cn/-c\d'))),
+        Rule(LinkExtractor(allow=('http://www-test.babyonlinedress.cn/-g\d')),callback='pass_item')
+    )
+    def parse_item(self, response):
+        infos1 = response.css('div.clothing-item')
+        for info in infos1:
+            l = DefaultItemLoader(item=BabyDetailItem(),selector=info)
+            l.add_value('cur_link', get_base_url(response))
+            l.add_css('name', '/html/head/title')
+            # print(l.load_item())
+            yield l.load_item()
 
-    def parse(self, response):
+
+    def parse1(self, response):
         infos1 = response.css('div.clothing-item')
         for info in infos1:
             l = DefaultItemLoader(item=BabyItem(),selector=info)
@@ -24,7 +40,6 @@ class BabydressSpider(scrapy.Spider):
             l.add_css('market_price', 'div.clothing-info span.price-big b::text')
             # print(l.load_item())
             yield l.load_item()
-
     def parse3(self, response):
         css = 'div.clothing-item div.clothing-info '
 
